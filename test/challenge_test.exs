8802cc751cb8adbcbd9400ef6f2a59923873f057
Mudassar <mudassar.elley@gmail.com>
2023-09-04 16:53:39 +0500

    update tests

--- test/challenge_test.exs
+++ test/challenge_test.exs
@@ -3,12 +3,23 @@ defmodule ChallengeTest do
 
   doctest Challenge.URLShortener
 
+  @url "www.google.com"
+
   test "generates shorten of valid url" do
-    assert Challenge.shorten("www.google.com") == "0a137b375cc3881a70e186ce2172c8d1"
+    assert "0a137b375cc3881a70e186ce2172c8d1" = Challenge.shorten(@url)
   end
 
   test "generates shorten of in valid url" do
     assert Challenge.shorten(nil) == {:error, :invalid_url}
+  end
+
+  test "gets a redirect url using valid shorten string" do
+    short = Challenge.shorten(@url)
+    assert @url = Challenge.get(short)
+  end
+
+  test "try getting redirect url using invalid shorten string" do
+    assert {:error, :url_not_found} = Challenge.get("example")
   end
 
   test "cleans up in-memory store" do
@@ -16,7 +27,7 @@ defmodule ChallengeTest do
   end
 
   test "counts the generated url" do
-    _shortened = Challenge.shorten("www.google.com")
+    _shortened = Challenge.shorten(@url)
     assert Challenge.count() > 0
   end
 end
